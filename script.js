78ac47b823d4d0dc1fe99381ba81eb04b56ac4d0
Prem935 <prem886044@gmail.com>
2026-04-14 15:27:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const bands = ["The Plot in You", "The Devil Wears Prada", "Pierce the Veil", "Norma Jean", "The Bled", "Say Anything", "The Midway State", "We Came as Romans", "Counterparts", "Oh, Sleeper", "A Skylit Drive", "Anywhere But Here", "An Old Dog"];
+
+function trimed(band) {
+	return band.replace(/^(a |an |the )/i, "").trim()
+}
+
+const sortedBand = bands.sort((a,b) => {
+	return trimed(a).localeCompare(trimed(b))
+})
+
+const ul = document.querySelector("#band")
+
+sortedBand.forEach(band => {
+	const li = document.createElement("li")
+	li.textContent = band
+	ul.appendChild(li)
+})
